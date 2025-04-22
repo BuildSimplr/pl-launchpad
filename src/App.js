a937967a0001f45fbba2042104181a880fbdac10
unknown <buildsimplr@gmail.com>
2025-04-22 08:50:42 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>🚀 PM Launchpad</h1>
+      <p>This is your AWS-powered product management toolkit.</p>
+    </div>
+  );
+}
+
+export default App;
